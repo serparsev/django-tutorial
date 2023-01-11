2953e2a7f856d1a6c6aba06dd71172e9ecda0331
Sergio Parra Sevillano <serparsev@gmail.com>
2023-01-11 15:41:52 +0100

    Created index and detail views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("pub_date")
+    print(latest_question_list, flush=True)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "index.html", context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render(request, "detail.html", context)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
